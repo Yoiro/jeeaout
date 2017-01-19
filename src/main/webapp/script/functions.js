5e71fa95372912e4c882d729851c6c1f043743f4
Alastard <xerud2003@hotmail.com>
2017-01-19 14:27:09 +0100

    Dynamic change of PrixTot

--- src/main/webapp/script/functions.js
+++ src/main/webapp/script/functions.js
@@ -1,15 +1,16 @@
-function updatePrixTotal(object){
+function updatePrixTotal(){
 	var listProduits=new Array();
 	var rows=$("#datatable").find('tr');
-	for(var i=0; i<rows.length;i++){
+	for(var i=1; i<rows.length;i++){
 		var r = $(rows[i]);
-		var nbProduitForThisRow=Number(r.find(".nbProduit").text());
+		var nbProduitForThisRow=Number(r.find(".nbProduit").val());
 		console.log("nbProduit: "+nbProduitForThisRow);
 		var number=Number(r.find(".prixUnit").text());
 		console.log("prixUnitaire: "+number);
 		var prixTot=nbProduitForThisRow*number;
-		r.find((".prixTot")).text("€ "+prixTot);
+		r.find((".prixTot")).text(prixTot);
+		console.log("Prix Total: "+prixTot);
 	}
 	//$("#prixTot").text("€ "+tmp);
-	console.log(prixTot);
+	
 }
