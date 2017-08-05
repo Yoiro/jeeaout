f5685ba382927745d05fe1a62bbae908df483d72
Alastard <xerud2003@hotmail.com>
2017-08-05 20:04:59 +0200

    add AllProductsController

---
+++ src/main/java/be/helha/groupe5/controllers/AllProductsController.java
@@ -0,0 +1,55 @@
+package be.helha.groupe5.controllers;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.view.ViewScoped;
+
+import be.helha.groupe5.daos.DAOPanierLocalBean;
+import be.helha.groupe5.daos.DAOProduitLocalBean;
+import be.helha.groupe5.entities.Panier;
+import be.helha.groupe5.entities.Produit;
+
+@ManagedBean
+@SessionScoped
+public class AllProductsController implements Serializable{
+	private List<Produit> produits;
+	private Panier panier;
+	
+	@EJB
+	private DAOProduitLocalBean daoProduitLocalBean;
+	
+	@EJB
+	private DAOPanierLocalBean daoPanierLocalBean;
+	
+	@PostConstruct
+	public void init() {
+		produits = daoProduitLocalBean.findAll();
+		panier = daoPanierLocalBean.create(new Panier());
+	}
+
+	//Getters & Setters
+	//----------------------------------------------------
+	public List<Produit> getProduits() {
+		return produits;
+	}
+
+	public void setProduits(List<Produit> produits) {
+		this.produits = produits;
+	}
+	//----------------------------------------------------
+	
+	public String addToCart(String id, String quantity) {
+		System.out.println(id);
+		System.out.println(quantity);
+		Integer prodId = Integer.parseInt(id);
+		Integer qte = Integer.parseInt(quantity);
+		Produit p = daoProduitLocalBean.findById((long)prodId);
+		panier.ajouterProduitPanier(p, qte);
+		return "./panier.xhtml";
+	}
+}
